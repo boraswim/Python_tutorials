b5c5a9b892d6665ce97e17e6458228033d8723b6
boraswim <sevimbora@gmail.com>
2023-10-08 00:04:41 +0300

    progress#0
    Hello World
    Math operators
    Data types

---
+++ 1math_operators.py
@@ -0,0 +1,26 @@
+# # is used for comment lines
+
+# 5 plus 3
+print(5+3)
+
+# 5 minus 3
+print(5-3)
+
+# 5 multiplied by 3
+print(5*3)
+
+# 5 divided by 3
+print(5/3)
+
+# 5 divided by 3 (no remainder)
+print(5//3)
+
+# 5 to the power of 3
+print(5**3)
+
+# 5 mod 3
+print(5%3)
+
+# If at least one of the numbers are a float
+# the result will be a float
+print(3+4+5+6+7.1)
---
+++ 2data_types.py
@@ -0,0 +1,15 @@
+# Variable name defining rules
+# Can not start with a number
+# Case sensitive (name != Name)
+
+# Assign value 10 to the variable "number"
+number = 10
+print(number)
+
+# Change value of the variable "number"
+number = 20
+print(number)
+
+# Add 30 to value of the variable "number"
+number += 30
+print(number)
