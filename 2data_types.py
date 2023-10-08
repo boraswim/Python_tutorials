57d8b152f3f0a819be83fa203594ecc07757b3c2
boraswim <sevimbora@gmail.com>
2023-10-08 23:59:29 +0300

    update on 2data_types.py

--- 2data_types.py
+++ 2data_types.py
@@ -13,3 +13,18 @@ print(number)
 # Add 30 to value of the variable "number"
 number += 30
 print(number)
+
+x = 1           # int
+y = 2.3         # float
+name = "Bora"   # string
+isDone = True   # bool
+
+x, y, name, isDone = (1, 2.3, "Bora", True) # Same command with lines 17-20
+
+a = 10
+b = 20
+print(a+b) # int sum (10 + 20 = 30)
+
+a = "10"
+b = "20"
+print(a+b) #string concat ("10" + "20" = "1020")
